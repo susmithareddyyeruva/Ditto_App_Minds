04b6c718ccff530542e1d5ba98790b336196ed54
livin.s <livin.s@infosys.com>
2021-03-30 14:11:31 +0530

    test package directory name changed in onboarding

--- onboarding/ui/src/androidTest/java/ditto/onboarding/ui/ExampleInstrumentedTest.kt
+++ onboarding/ui/src/androidTest/java/ditto/onboarding/ui/ExampleInstrumentedTest.kt
@@ -1,13 +1,10 @@
-package trace.onboarding.ui
+package ditto.onboarding.ui
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("trace.onboarding.ui.test", appContext.packageName)
+        assertEquals("ditto.onboarding.ui.test", appContext.packageName)
     }
 }
