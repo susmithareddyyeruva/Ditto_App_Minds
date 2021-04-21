b1ca5cb3b6890d303b0c934f23c49540c61bdc8c
livin.s <livin.s@infosys.com>
2021-04-21 19:35:07 +0530

    set the url taken from build config

--- core-lib/src/main/java/core/Constants.kt
+++ core-lib/src/main/java/core/Constants.kt
@@ -1,6 +1,8 @@
 package core
 
-internal const val BASE_URL = "https://dev05-na03-joann.demandware.net/s/ditto/dw/shop/v19_1/"
+import core.lib.BuildConfig
+
+internal const val BASE_URL = BuildConfig.BASEURL
 const val NO_NETWORK = "Network connection required !"
 internal const val MOCK_API_CERT = "mockapicert.crt"
 const val GUEST_SIGNED_IN = "GUEST"
