657db2ec5cc64a7510fa2f8e9c6b203664ddc535
livin.s <livin.s@infosys.com>
2021-05-03 12:57:35 +0530

    usecase updated

--- login/domain/src/main/java/com/ditto/login/domain/UseCases.kt
+++ login/domain/src/main/java/com/ditto/login/domain/UseCases.kt
@@ -7,7 +7,6 @@ import non_core.lib.Result
 interface GetLoginDbUseCase {
     fun invoke(): Single<Result<LoginUser>>
     fun createUser(user: LoginUser): Single<Long>
-    fun userLogin(user: LoginUser):Single<Result<LoginUser>>
     fun loginUserWithCredential(user: LoginInputData): Single<Result<LoginResultDomain>>
 
 }
--- splash/domain/src/main/java/com/ditto/splash/domain/UseCases.kt
+++ splash/domain/src/main/java/com/ditto/splash/domain/UseCases.kt
@@ -7,6 +7,8 @@ import non_core.lib.Result
 
 interface GetDbDataUseCase {
     fun getUser(): Single<Result<LoginUser>>
+
+    fun deleteDbUser(user: LoginUser):Single<Boolean>
 }
 interface UpdateDbUseCase {
     fun invoke(): Single<Any>
