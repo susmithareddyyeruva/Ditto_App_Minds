ecbf866fe7e4d15647e9f7aeeeb25037a32b94a0
livin.s <livin.s@infosys.com>
2021-05-27 17:46:00 +0530

    set toolbar in FaqGlossaryMainFragment.kt

--- menuitems/ui/src/main/java/com/ditto/menuitems_ui/faq/ui/FaqGlossaryMainFragment.kt
+++ menuitems/ui/src/main/java/com/ditto/menuitems_ui/faq/ui/FaqGlossaryMainFragment.kt
@@ -1,19 +1,21 @@
 package com.ditto.menuitems_ui.faq.ui
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.ditto.menuitems_ui.databinding.FaqGlossaryMainfragmentBinding
 import com.ditto.menuitems_ui.faq.ui.adapters.TabFaqAdapter
 import com.google.android.material.tabs.TabLayout
 import core.ui.BaseFragment
-import core.ui.BottomNavigationActivity
 import core.ui.ViewModelDelegate
 
-class FaqGlossaryMainFragment() : BaseFragment() {
+class FaqGlossaryMainFragment : BaseFragment() {
 
     private val viewModel: FAQGlossaryfragmentViewModel by ViewModelDelegate()
     lateinit var binding: FaqGlossaryMainfragmentBinding
@@ -30,6 +32,7 @@ class FaqGlossaryMainFragment() : BaseFragment() {
         return binding.mainContainer
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         setuptoolbar()
@@ -38,12 +41,11 @@ class FaqGlossaryMainFragment() : BaseFragment() {
     }
 
     private fun setuptoolbar() {
-        bottomNavViewModel.visibility.set(false)
         toolbarViewModel.isShowTransparentActionBar.set(false)
-        toolbarViewModel.isShowActionBar.set(true)
-        toolbarViewModel.isShowActionMenu.set(false)
-        (activity as BottomNavigationActivity).hidemenu()
-        (activity as BottomNavigationActivity).setToolbarTitle("FAQ and Glossary")
+        toolbarViewModel.isShowActionBar.set(false)
+        bottomNavViewModel.visibility.set(false)
+        (activity as? AppCompatActivity)?.setSupportActionBar(binding.toolbar)
+        (activity as AppCompatActivity?)?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     private fun setTabsAdapter() {
