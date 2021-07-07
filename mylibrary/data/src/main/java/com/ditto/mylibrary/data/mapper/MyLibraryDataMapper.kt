63ea2992e4a493f9ab7fb72ff1e8e898ba9ed9e3
livin.s <livin.s@infosys.com>
2021-07-07 15:38:37 +0530

    modified response

--- mylibrary/data/src/main/java/com/ditto/mylibrary/data/mapper/MyLibraryDataMapper.kt
+++ mylibrary/data/src/main/java/com/ditto/mylibrary/data/mapper/MyLibraryDataMapper.kt
@@ -53,15 +53,24 @@ fun MyLibraryResult.toDomain(): AllPatternsDomain {
 
 fun Prod.toDomain(): ProdDomain {
     return ProdDomain(
-        ID = this.iD,
+        iD = this.iD,
         prodBrand = this.prodBrand,
         prodGender = this.prodGender,
         prodName = this.prodName,
         patternType = this.patternType,
         prodSize = this.prodSize,
         tailornovaDesignId = this.tailornovaDesignId,
-        tailornovaDesignName = this.tailornovaDesignName,
-        image = this.image
+        creationDate = this.creationDate,
+        image = this.image,
+        customization = this.customization,
+        dateOfModification = this.dateOfModification,
+        longDescription = this.longDescription,
+        occasion = this.occasion,
+        season = this.season,
+        status = this.status,
+        subscriptionExpiryDate = this.subscriptionExpiryDate,
+        suitableFor = this.suitableFor,
+        type = this.type
 
     )
 }
