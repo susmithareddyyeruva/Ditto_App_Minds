4bf6e0b03e135a1e570766704af7c61647673bc6
Nivedh Kallekkad Suresh <nivedh.suresh@ad.infosys.com>
2021-10-21 16:32:08 +0530

    Calibration message change

--- calibration/ui/src/main/java/com/ditto/calibration/ui/CalibrationFragment.kt
+++ calibration/ui/src/main/java/com/ditto/calibration/ui/CalibrationFragment.kt
@@ -778,8 +778,8 @@ class CalibrationFragment : BaseFragment(), Utility.CallbackDialogListener, Util
         Utility.showAlertDialogue(
             requireContext(),
             R.drawable.ic_calibration_failure,
+            message,
 //            String.format(getString(R.string.calibration_failure), message),
-            String.format("", message),
             "TUTORIAL",
             "RETRY",
             "SKIP CALIBRATION",
