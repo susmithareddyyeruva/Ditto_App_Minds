ab5b94422e2ba215ecf511600c6dcd792b362b44
livin.s <livin.s@infosys.com>
2021-12-31 12:07:55 +0530

    hamberger menu showing in login while launch app via deeplink issue resolved in SplashFragment.kt

--- splash/ui/src/main/java/com/ditto/splash/ui/SplashFragment.kt
+++ splash/ui/src/main/java/com/ditto/splash/ui/SplashFragment.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.ditto.logger.Logger
@@ -55,6 +54,9 @@ class SplashFragment : BaseFragment(),Utility.CustomCallbackDialogListener {
         super.onActivityCreated(savedInstanceState)
         bottomNavViewModel.visibility.set(false)
         toolbarViewModel.visibility.set(false)
+        toolbarViewModel.isShowTransparentActionBar.set(false)
+        toolbarViewModel.isShowActionBar.set(true)
+        toolbarViewModel.isShowActionMenu.set(false)
         viewModel.disposable += viewModel.events
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
