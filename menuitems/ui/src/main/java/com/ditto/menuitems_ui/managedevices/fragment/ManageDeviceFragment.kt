196ea6456ff28c45ad109036ed2c3a2171e6b98a
nivedh.suresh <nivedh.suresh@infosys.com>
2022-09-30 11:35:57 +0530

    Calibration on Switching Fixed

--- menuitems/ui/src/main/java/com/ditto/menuitems_ui/managedevices/fragment/ManageDeviceFragment.kt
+++ menuitems/ui/src/main/java/com/ditto/menuitems_ui/managedevices/fragment/ManageDeviceFragment.kt
@@ -495,6 +495,8 @@ class ManageDeviceFragment : BaseFragment(), Utility.CustomCallbackDialogListene
 
         when (alertType) {
             Utility.AlertType.SOC_CONNECT -> {
+                //viewModel.disConnectToProjector(NetworkUtility.nsdSericeHostName,NetworkUtility.nsdSericePortName,false)
+                viewModel.isCalibrated.set(false)
                 viewModel.sendWaitingImage()
                 viewModel.connectToProjector(
                     receivedServiceList!![viewModel.clickedPosition.get()].nsdSericeHostAddress,
