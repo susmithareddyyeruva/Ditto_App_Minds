bcb74f306fe937c876042e70b438a4ccf0374194
AparnaZope <aparna.zope@infosys.com>
2023-01-23 17:12:05 +0530

    - added new folder icons as per new branding pdf

--- mylibrary/ui/src/main/java/com/ditto/mylibrary/ui/MyFolderViewModel.kt
+++ mylibrary/ui/src/main/java/com/ditto/mylibrary/ui/MyFolderViewModel.kt
@@ -272,17 +272,17 @@ class MyFolderViewModel @Inject constructor(private val myLibraryUseCase: MyLibr
     private fun handleFolderFetchResult(folderResult: Result<FoldersResultDomain>?) {
         folderList = arrayListOf(
             MyFolderData(
-                R.drawable.ic_newfolder,
+                R.drawable.add_folder_icon,
                 "Create New Folder",
                 false
             ),
             MyFolderData(
-                R.drawable.ic_owned,
+                R.drawable.my_folder_icon,
                 "Owned",
                 false
             ),
             MyFolderData(
-                null,
+                R.drawable.my_folder_icon,
                 "Favorites",
                 false
             )
@@ -292,7 +292,7 @@ class MyFolderViewModel @Inject constructor(private val myLibraryUseCase: MyLibr
                 folderResult.data.responseStatus.forEach {
                     folderList.add(
                         MyFolderData(
-                            url = null,
+                            url = R.drawable.my_folder_icon,
                             title = it,
                             isAction = true
                         )
